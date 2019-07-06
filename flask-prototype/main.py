7ce723c9c6b996d1abe7b21d87a29776815e00e4
spcogg <spcogg@gmail.com>
2019-07-07 00:17:11 +1000

    simplified webapp skeleton

--- flask-prototype/main.py
+++ flask-prototype/main.py
@@ -1,1 +1,14 @@
-from app import app
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return "<h1> Home Page </h1>"
+
+@app.route("/about")
+def about():
+    return "About"
+
+if __name__ == '__main__':
+    app.run(debug=True)
