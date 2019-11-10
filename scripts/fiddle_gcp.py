3b1ecde7a245e505c22a452bf1a81500d47d6b0c
hungy <ye.hansang@gmail.com>
2019-11-10 22:59:22 +1100

    refactored recommender, added and tested GCP cloud storage backend option

---
+++ scripts/fiddle_gcp.py
@@ -0,0 +1,52 @@
+from google.cloud import storage
+from os import listdir
+from os.path import isfile, join
+
+bucket_name = "travis_recommender"
+bucket_folder = "csv_data\\"  # doesn't work cos gcp is unix
+bucket_folder = "csv_data/"  # the correct way for unix
+file_name = "locations_recommender.csv"
+credentials="AIzaSyAGUOIBkPjUpQmf27UXBR4HkbuRt1i4Cmw"
+project = "travis-test-01"
+json_keyfile = 'Travis-test-01-35fea3db5a86.json'
+storage_client = storage.Client.from_service_account_json(json_keyfile)
+
+bucket = storage_client.get_bucket(bucket_name)  # now it will create bucket obj
+
+local_folder = "travis_optimiser\\test_data\\"
+def upload_files(bucket_name, bucket, local_folder):
+    """Upload files to GCP bucket."""
+    # files = [f for f in listdir(local_folder) if isfile(join(local_folder, f))]
+    file = 'locations.csv'
+    localFile = local_folder + file
+    blob = bucket.blob(bucket_folder + file)
+    blob.upload_from_filename(localFile)
+    return f'Uploaded {file} to "{bucket_name}" bucket.'
+
+upload_files(bucket_name, bucket, local_folder)
+
+def list_files(bucket_name, bucket_folder):
+    """List all files in GCP bucket."""
+    files = bucket.list_blobs(prefix=bucket_folder)
+    fileList = [file.name for file in files if '.' in file.name]
+    return fileList
+
+list_files(bucket_name, bucket_folder)
+
+def download_file(bucket):
+    bucket_folder = "csv_data/"  # the correct way for unix
+    file = "locations_recommender.csv"
+    blob = bucket.blob(bucket_folder + file)
+    filename = blob.name.split('/')[-1]
+    local_folder = "scripts"
+    dl_path = join(local_folder, filename)
+    blob.download_to_filename(dl_path)
+    return f'{filename} downloaded from bucket.'
+
+download_file(bucket)  # will download the csv to bucket
+
+def delete_file(bucket_name, bucket_folder, filename):
+    """Delete file from GCP bucket."""
+    bucket.delete_blob(bucket_folder + filename)
+    return f'{filename} deleted from bucket.'
+
--- tests/test_recommender.py
+++ tests/test_recommender.py
@@ -1,5 +1,9 @@
-from travis_optimiser.recommender import rec_search_gmaps_at_latlon, rec_search_at_latlon_point, rec_search_list_at_latlon, get_best_recs, get_gmaps, rec_from_list
-from travis_optimiser.recommender_data import get_df_loc
+from travis_optimiser.recommender import rec_search_gmaps_at_latlon, rec_search_list_at_latlon, get_best_recs, get_gmaps, rec_from_list
+# from travis_optimiser.recommender_data import get_df_loc
+from travis_optimiser.recommender import get_df_loc
+from utils import utilities
+
+cfg = utilities.get_cfg()
 
 def test_rec_from_list():
     # NOTE TESTING PASS
@@ -13,15 +17,15 @@ def test_rec_from_list():
 def test_search_gmap_at_latlon():
     # NOTE: Testing pass
     target_lat_lon = (-37.81493625, 144.96061525)
-    rectype = 'eat'
+    # rectype = 'eat'
     gmaps = get_gmaps()
-    dfLoc = get_df_loc()
+    # dfLoc = get_df_loc(method='local')
     rec_search_gmaps_at_latlon(gmaps, target_lat_lon, rectype='restaurant')
 
 def test_search_list_at_latlon():
     # NOTE: Testing Pass
     target_lat_lon = (-37.81493625, 144.96061525)
-    rectype = 'eat'  # NOTE: Rectype different for built in vs gmaps
+    # rectype = 'eat'  # NOTE: Rectype different for built in vs gmaps
     dfLoc = get_df_loc()
     test_result = rec_search_list_at_latlon(dfLoc, target_lat_lon, rectype='eat')
     print(test_result)
@@ -34,9 +38,11 @@ def test_get_best_recs():
         "ChIJdedaLk5d1moRQOX0CXZWBB0",  # sthn cross
         "ChIJczgQh8lC1moR9r9gP44FRvY",  # chinatown
     ]
-    test_result = get_best_recs(gmaps=gmaps, input_gpids=input_gpids, rectype=rectype)
+    cfg = utilities.get_cfg()
+    test_result = get_best_recs(gmaps=gmaps, input_gpids=input_gpids, rectype=rectype,
+                                cfg=cfg)
     print(test_result)
 
 if __name__ == "__main__":
-    test_search_list_at_latlon()
+    # test_search_list_at_latlon()
     test_get_best_recs()
